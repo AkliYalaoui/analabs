b4d1a69a082dffc938ccf093bd68f907d628c041
AkliYalaoui <akliyalaoui@gmail.com>
2022-09-04 03:40:47 +0100

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import "@fontsource/roboto/700.css";
 import "../styles/globals.css";
 
 import Head from "next/head";
+import Script from "next/script";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
@@ -21,6 +22,19 @@ export default function MyApp(props) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-L0ZL6LGTNJ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-L0ZL6LGTNJ');
+        `}
+      </Script>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Layouts>
